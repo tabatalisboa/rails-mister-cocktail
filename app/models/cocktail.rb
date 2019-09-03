79518df98ab2fece24454360ff8ad1ab6fa3bf33
tabatalisboa <tabatalisboa@pm.me>
2019-09-03 18:26:29 -0300

    Add seed for ingredients via JSON API

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -1,4 +1,6 @@
 class Cocktail < ApplicationRecord
   has_many :doses
   has_many :ingredients, through: :doses
+
+  validates :name, presence: true, uniqueness: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+require 'open-uri'
+require 'json'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +7,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Create ingredients'
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+
+serialized_ingredients = open(url).read
+ingredients = JSON.parse(serialized_ingredients)
+
+ingredients['drinks'].each do |ingredient|
+  one_ingredient = Ingredient.create(name: ingredient['strIngredient1'])
+  puts "create #{one_ingredient.name}"
+end
