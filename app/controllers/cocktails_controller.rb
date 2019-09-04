908fb1a7220369ba514e9dccc510d2ccd636aedd
tabatalisboa <tabatalisboa@pm.me>
2019-09-04 16:20:01 -0300

    Complete Cocktails Controller's actions

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,15 +1,24 @@
 class CocktailsController < ApplicationController
-  :index, :new, :create, :show]
   def index
-  end
-
-  def new
-  end
-
-  def create
+    @cocktails = Cocktail.all
   end
 
   def show
+    @cocktail = Cocktail.find(params[:id])
+  end
+
+  def new
+    @cocktail = Cocktail.new
+  end
+
+  def create
+    @cocktail = Cocktail.create(cocktail_params)
+    redirect_to cocktails_path
+  end
+
+  private
+
+  def cocktail_params
+    params.require(:cocktail).permit(:name)
   end
 end
-
