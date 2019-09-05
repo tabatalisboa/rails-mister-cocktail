272302a1f4f69c070c1d50b892a4ac14b9dd1d3f
tabatalisboa <tabatalisboa@pm.me>
2019-09-04 21:11:03 -0300

    Create a Doses Controller

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -1,6 +1,6 @@
 # typed: strong
 class Cocktail < ApplicationRecord
-  has_many :doses
+  has_many :doses, dependent: :destroy
   has_many :ingredients, through: :doses
 
   validates :name, presence: true, uniqueness: true
